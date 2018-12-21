080453851e32315b9a3e117d7f422b15ac4809a3
Ruben Rizzi <raynor85@gmail.com>
2018-12-21 22:42:13 +0000

    Fix build

--- ios/RNHexagonView-Bridging-Header.h
+++ ios/RNHexagonView-Bridging-Header.h
@@ -1,6 +1,8 @@
 #ifndef RNHexagonView_Bridging_Header_h
 #define RNHexagonView_Bridging_Header_h
 
-#import "React/RCTBridgeModule.h"
+#import <React/RCTBridgeModule.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTConvert.h>
 
 #endif /* RNHexagonView_Bridging_Header_h */
